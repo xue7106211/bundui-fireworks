944594b40e765a12559378e56ac6f1603195f482
薛困惑 <xue7106211@gmail.com>
2025-07-19 17:18:05 +0800

    修复背景高度

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import FireworksBackground from "@/components/core/text-gradient-scroll";
 
 export default function FireworksBackgroundExample() {
   return (
-    <FireworksBackground className="flex aspect-16/9 items-center justify-center">
+    <FireworksBackground className="flex min-h-screen items-center justify-center">
       <div className="z-10 space-y-4 text-center lg:space-y-6">
-        <h4 className="text-2xl font-semibold text-white/80 lg:text-3xl">AllenXue Components</h4>
+        <h4 className="text-2xl font-semibold text-white/80 lg:text-3xl">AllenXue Design</h4>
         {/* <Button className="bg-white! text-black">Discover Excellence</Button> */}
       </div>
     </FireworksBackground>
